65cb499872d5c514d529e8954e1c33cfe93c251a
Kentonhoff <44037845+Kentonhoff@users.noreply.github.com>
2019-11-24 18:11:55 -0500

    Finished challenge 3

--- js/challenge3.js
+++ js/challenge3.js
@@ -1,16 +1,37 @@
-ch3form.onsubmit = function() {
-    var fruit = document.querySelectorAll('input[name="fruit"]')
-    for (let i = 0; i < fruit.length;i++){
-        if (fruit[i].checked)
-            return true;
+$(document).ready(function(){
+  $("#ch3form").submit(function(){
+    var fruit = $('input[name="fruit"]')
+    for (let i = 0; i< fruit.length;i++){
+      if (fruit[i].checked)
+        return true;
     }
-    alert("You must pick a fruit!")
-    return false;
+    alert("Pick a fruit please.")
+      return false;
+  })
+  $("#ch3form").submit(function(){
+    var standing = $('input[name="standing"]')
+    for (let i = 0; i< standing.length;i++){
+      if (standing[i].checked)
+        return true;
+    }
+    alert("Pick a class standing please.")
+      return false;
+  })
+})
+
+//ch3form.onsubmit = function() {
+//    var fruit = document.querySelectorAll('input[name="fruit"]')
+//    for (let i = 0; i < fruit.length;i++){
+//        if (fruit[i].checked)
+//            return true;
+//    }
+//    alert("You must pick a fruit!")
+//    return false;
 
     // Advanced Answer
     // if(document.querySelector('input[name="fruit"]:checked'))
     //     return true;
-    
+
     // alert("You must pick a fruit!")
     // return false;
-  }
+//  }
